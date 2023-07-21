d57a3bef42cdc5d919cba061bbccca669eff1e3e
GlebMudrov <mudrov.gleb@gmail.com>
2023-07-21 19:20:56 +0300

    v 1.1

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -14,7 +14,7 @@ from .settings import SHORT_URL_PATTERN
 def get_original_url(short_id: str):
     original_url = URLMap.query.filter_by(short=short_id).first()
     if not original_url:
-        raise InvalidAPIUsage('Данный id не найден', HTTPStatus.NOT_FOUND)
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
     return jsonify({'url': original_url.original}), HTTPStatus.OK
 
 
@@ -23,12 +23,11 @@ def add_id():
     data = request.get_json()
     if not data:
         raise InvalidAPIUsage('Отсутствует тело запроса', HTTPStatus.BAD_REQUEST)
-    if not data.get('url'):
-        raise InvalidAPIUsage('"url" является обязательным полем')
-
+    if 'custom_id' not in data or not data['custom_id']:
+        data["custom_id"] = get_unique_short_id()
     short_id = data.get('custom_id')
-    if not short_id:
-        short_id = get_unique_short_id()
+    if not data.get('url'):
+        raise InvalidAPIUsage('"url" является обязательным полем!')
     if not re.search(SHORT_URL_PATTERN, short_id) or len(short_id) > 16:
         raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
     if URLMap.query.filter_by(short=short_id).first():
@@ -38,4 +37,5 @@ def add_id():
     urlmap.from_dict(data)
     db.session.add(urlmap)
     db.session.commit()
+    
     return jsonify(urlmap.to_dict()), HTTPStatus.CREATED
